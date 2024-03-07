a11d349446ee66cf0e18e6da5d0ab631a3589495
LAB11_USER ON WS26 <L11Y08W26@citulab.net>
2024-03-07 09:58:12 +0800

    Added the calculator layout

--- app/src/main/java/com/example/androidprojectcollection/CalculatorActivity.java
+++ app/src/main/java/com/example/androidprojectcollection/CalculatorActivity.java
@@ -3,6 +3,9 @@ package com.example.androidprojectcollection;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CalculatorActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class CalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+    }
+
+    public void addToDisplay(View v){
+        TextView equation = findViewById(R.id.equation);
+        Button button = (Button) v;
+        String buttonText = button.getText().toString();
+
+        // Assuming the button text is the number you want to add
+        String currentText = equation.getText().toString();
+        String newText = currentText + buttonText;
+
+        equation.setText(newText);
     }
 }
